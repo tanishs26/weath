8e6322f45d00254b5774f44cc0ec53fbbd79607d
tanishs26 <tanishsharma2627@gmail.com>
2025-01-25 19:24:02 +0530

    added logic and images for weather condition

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const max = document.querySelector("#max");
 const statu = document.querySelector("#status");
 const btn = document.querySelector(".search-btn");
 const inp = document.querySelector("#primary-searchbar");
+const weather_card = document.querySelector(".weather-card");
+const weather_img = document.querySelector("#weath-type-img");
 const key = "";
 btn.addEventListener("click", () => {
   let city = inp.value;
@@ -25,11 +27,11 @@ btn.addEventListener("click", () => {
     max.innerHTML = ` ${dataFetched.main.temp_max}`;
     statu.innerHTML = `• ${dataFetched.weather[0].main}`;
     const timeStamp = dataFetched.dt;
-    const newDate = new Date(timeStamp * 1000);
+    const newDate = new Date((timeStamp + dataFetched.timezone) * 1000);
     console.log(newDate);
-    const day = newDate.getDate();
-    const month = newDate.getMonth() + 1;
-    const year = newDate.getFullYear();
+    const day = newDate.getUTCDate();
+    const month = newDate.getUTCMonth() + 1;
+    const year = newDate.getUTCFullYear();
     const daysOfWeek = [
       "Sunday",
       "Monday",
@@ -39,10 +41,10 @@ btn.addEventListener("click", () => {
       "Friday",
       "Saturday",
     ];
-    const dayOfWeek = daysOfWeek[newDate.getDay()];
+    const dayOfWeek = daysOfWeek[newDate.getUTCDay()];
     date.innerHTML = ` ${dayOfWeek} ${month}/${day}/${year}`;
-    const minutes = newDate.getMinutes();
-    const hours = newDate.getHours();
+    const minutes = newDate.getUTCMinutes();
+    const hours = newDate.getUTCHours();
     time.innerHTML = `${hours}:${minutes < 10 ? "0" + minutes : minutes}`;
 
     //Today's Details Section:
@@ -51,10 +53,47 @@ btn.addEventListener("click", () => {
     const speed = document.querySelector("#speed");
     const pressure = document.querySelector("#air-pressure");
 
-    feel.innerHTML=`${dataFetched.main.feels_like}°c`;
-    humidity.innerHTML=`${dataFetched.main.humidity}%`;
-    speed.innerHTML=`${dataFetched.wind.speed}Km/h`;
-    pressure.innerHTML=`${dataFetched.main.pressure}hPa`
+    feel.innerHTML = `${dataFetched.main.feels_like}°c`;
+    humidity.innerHTML = `${dataFetched.main.humidity}%`;
+    speed.innerHTML = `${dataFetched.wind.speed}Km/h`;
+    pressure.innerHTML = `${dataFetched.main.pressure}hPa`;
+    //Image Section
+    if (hours >= 19 || hours < 6) {
+      weather_card.style.backgroundImage = "url(Background/night.png)";
+      if (dataFetched.weather[0].main == "Rain") {
+        weather_img.src = "icons/RainNight.svg";
+      }
+      if (dataFetched.weather[0].main == "Clear") {
+        weather_img.src = "icons/clearNight.svg";
+      }
+      if (dataFetched.weather[0].main == "Clouds"||dataFetched.weather[0].main == "Haze") {
+        weather_img.src = "icons/cloudsNight.svg";
+      }
+      if (dataFetched.weather[0].main == "Snow") {
+        weather_img.src = "icons/snow.svg";
+      }
+      if (dataFetched.weather[0].main == "Thunderstorm") {
+        weather_img.src = "icons/stormNight.svg";
+      }
+    } else {
+      weather_card.style.backgroundImage = "url(Background/clouds.png)";
+      if (dataFetched.weather[0].main == "Rain") {
+        weather_img.src = "icons/Rain.svg";
+      }
+      if (dataFetched.weather[0].main == "Clear") {
+        weather_img.src = "icons/clear.svg";
+      }
+      if (dataFetched.weather[0].main == "Clouds"||dataFetched.weather[0].main == "Haze") {
+        weather_img.src = "icons/clouds.svg";
+      }
+      if (dataFetched.weather[0].main == "Snow") {
+        weather_img.src = "icons/snow.svg";
+      }
+      if (dataFetched.weather[0].main == "Thunderstorm") {
+        weather_img.src = "icons/storm.svg";
+      }
+    }
   }
+
   getWeather();
 });
